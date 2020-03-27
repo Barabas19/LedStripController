21ff2ef5a87c871ca9463ddb0daee28bcfe12a5c
Barabas19 <naxer06@gmail.com>
2020-03-27 10:19:18 +0100

    finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define DDEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+//#define DDEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
 #ifdef DDEBUG    //Macros are usually in all capital letters.
   #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
   #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
